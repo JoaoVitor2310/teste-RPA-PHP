3e8a5b274f47391ca59a5223477ecb94ae04323f
João Vitor CF <joaovitormatosgouveia@gmail.com>
2025-02-04 12:39:40 -0300

    feat: eagle automação

---
+++ eaglecred/eagle.php
@@ -0,0 +1,174 @@
+<?php
+
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    session_start();
+
+    // 1 - Acesar pagina inicial para gerar cookies
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://webcred.eaglecred.com.br/',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+        CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+        CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
+    ));
+
+    $response = curl_exec($curl);
+
+    // Validar
+    if (!$response) {
+        echo 'Erro cURL: ' . curl_error($curl);
+        $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
+        header("Location: index.php");
+        exit();
+    }
+
+    if (preg_match("/var\s+hashv\s*=\s*'([^']+)'/", $response, $matches)) {
+        $hashv = $matches[1];
+        // echo "Valor de hashv: $hashv";
+    } else {
+        echo 'Erro cURL: ' . curl_error($curl);
+        $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
+        header("Location: index.php");
+        exit();
+    }
+
+
+    curl_close($curl);
+    // echo $hashv;
+    // exit();
+
+
+    // 2 - Realizar login
+    // $curl = curl_init();
+    // curl_setopt_array($curl, array(
+    //     CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/passwordRules.php', // https://webcred.eaglecred.com.br/ajx/login.php
+    //     CURLOPT_RETURNTRANSFER => true, // 
+    //     CURLOPT_ENCODING => '',
+    //     CURLOPT_MAXREDIRS => 10,
+    //     CURLOPT_TIMEOUT => 0,
+    //     CURLOPT_FOLLOWLOCATION => true,
+    //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //     CURLOPT_CUSTOMREQUEST => 'POST',
+    //     CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+    //     CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+    //     CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
+    //     CURLOPT_POSTFIELDS => array(
+    //         'l' => $_POST["email"],
+    //         // 'l' => 'CACI26',
+    //         's' => $_POST["password"],
+    //         // 's' => 'Futuro@123',
+    //         'r' => '1',
+    //         'v' => $hashv,
+    //     ),
+    // ));
+
+    // $response = curl_exec($curl);
+    // // exit($response);
+
+    // // Validar
+    // if (!$response) {
+    //     echo 'Erro cURL: ' . curl_error($curl);
+    //     $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
+    //     header("Location: index.php");
+    //     exit();
+    // }
+
+    // $data = json_decode($response, true);
+
+    // if ($data['error']) {
+    //     $_SESSION['error_message'] = 'Usuário ou senha incorretos. Tente novamente.';
+    //     header("Location: index.php");
+    //     exit();
+    // }
+
+    // // echo $response;
+    // curl_close($curl);
+
+    // 2 - Realizar login novamente
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/login.php', // https://webcred.eaglecred.com.br/ajx/login.php
+        CURLOPT_RETURNTRANSFER => true, // 
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+        CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+        CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
+        CURLOPT_POSTFIELDS => array(
+            'l' => $_POST["email"],
+            's' => $_POST["password"],
+            'r' => '1',
+            'v' => $hashv,
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    // exit($response); 
+
+
+
+
+
+    // Validar se enviou produtos
+    if ($_POST["products"] == "") {
+        $_SESSION['error_message'] = 'Nenhum produto foi enviado. Tente novamente.';
+        header("Location: index.php");
+        exit();
+    }
+
+    // Separar o id dos produtos em um array
+    $produtos = str_replace("NOME - ", "", $_POST["products"]);
+    $produtos = explode(',', $produtos);
+
+    // 2 - Permitir todos os usuários para o produto
+    foreach ($produtos as $produto) {
+        $produto = str_pad($produto, 6, "0", STR_PAD_LEFT);
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/ajx_parametrizaproduto.php',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+            CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+            CURLOPT_POSTFIELDS => '{
+            "CodProduto": "' . $produto . '",
+            "usuarios": [],
+            "todosForamSelecionados": true,
+            "process": "permitiremlote"
+        }',
+        ));
+
+        $response = curl_exec($curl);
+        // var_dump($response);
+        // die;
+
+        if (!$response) {
+            $_SESSION['error_message'] = 'Erro ao autorizar usuários. Tente novamente.';
+            header("Location: index.php");
+            exit();
+        }
+
+        // var_dump($response);
+        curl_close($curl);
+    }
+    $_SESSION['success_message'] = 'Usuários de produtos permitidos com sucesso.';
+    header("Location: index.php");
+    exit();
+}
---
+++ eaglecred/index.php
@@ -0,0 +1,77 @@
+<?php
+session_start();  // Inicie a sessão
+
+// Verifique se há uma mensagem de erro e a exiba
+if (isset($_SESSION['error_message'])) {
+    echo '<div class="alert alert-danger">' . $_SESSION['error_message'] . '</div>';
+    unset($_SESSION['error_message']);
+}
+
+if (isset($_SESSION['success_message'])) {
+    echo '<div class="alert alert-success">' . $_SESSION['success_message'] . '</div>';
+    unset($_SESSION['success_message']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Eaglecred</title>
+</head>
+
+<body>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="container text-center mx-auto p-4 bg-primary" style="max-width: 350px;">
+
+            <form action="./eagle.php" method="post">
+                <div class="d-flex flex-column align-items-center gap-2">
+                    <h1 class="text-white">Automação Eaglecred</h1>
+                    <input value="CACI26" class="row form-control" type="text" name="email" placeholder="Email">
+                    <div class="input-group">
+                        <input value="Futuro@123" id="password" class="form-control" type="password" name="password"
+                            placeholder="Senha">
+                        <button class="btn btn-outline "
+                            style="border-color: var(--bs-tertiary-bg); color: var(--bs-tertiary-bg);" type="button"
+                            onclick="togglePassword()">
+                            <i id="toggleIcon" class="bi bi-eye"></i>
+                        </button>
+                    </div>
+                    <textarea class="form-control mt-2" name="products" placeholder="Produtos (separados por vírgula)"
+                        rows="4">NOME - 12,16</textarea>
+                    <button type="submit" class="btn mt-2 form-control" style="background-color: white;">Enviar</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+
+    <script>
+        function togglePassword() {
+            const passwordInput = document.getElementById("password");
+            const icon = document.getElementById("toggleIcon");
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                icon.classList.remove("bi-eye");
+                icon.classList.add("bi-eye-slash");
+            } else {
+                passwordInput.type = "password";
+                icon.classList.remove("bi-eye-slash");
+                icon.classList.add("bi-eye");
+            }
+        }
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
