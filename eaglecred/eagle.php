5ee55bfaa7b4eb02ab146106edb24e833ee0a869
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-02-07 12:03:48 -0300

    feat: segunda automacao eaglecred

--- eaglecred/eagle.php
+++ eaglecred/eagle.php
@@ -1,137 +1,10 @@
 <?php
+session_start();
 
-if (isset($_POST["email"]) && isset($_POST["password"])) {
-    session_start();
+if (isset($_POST["form_type"]) && $_POST["form_type"] === 'produtos') {
 
-    // 1 - Acesar pagina inicial para gerar cookies
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://webcred.eaglecred.com.br/',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
-        CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
-        CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
-    ));
-
-    $response = curl_exec($curl);
-
-    // Validar
-    if (!$response) {
-        echo 'Erro cURL: ' . curl_error($curl);
-        $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
-        header("Location: index.php");
-        exit();
-    }
-
-    if (preg_match("/var\s+hashv\s*=\s*'([^']+)'/", $response, $matches)) {
-        $hashv = $matches[1];
-        // echo "Valor de hashv: $hashv";
-    } else {
-        echo 'Erro cURL: ' . curl_error($curl);
-        $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
-        header("Location: index.php");
-        exit();
-    }
-
-
-    curl_close($curl);
-    // echo $hashv;
-    // exit();
-
-
-    // 2 - Realizar login
-    // $curl = curl_init();
-    // curl_setopt_array($curl, array(
-    //     CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/passwordRules.php', // https://webcred.eaglecred.com.br/ajx/login.php
-    //     CURLOPT_RETURNTRANSFER => true, // 
-    //     CURLOPT_ENCODING => '',
-    //     CURLOPT_MAXREDIRS => 10,
-    //     CURLOPT_TIMEOUT => 0,
-    //     CURLOPT_FOLLOWLOCATION => true,
-    //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //     CURLOPT_CUSTOMREQUEST => 'POST',
-    //     CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
-    //     CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
-    //     CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
-    //     CURLOPT_POSTFIELDS => array(
-    //         'l' => $_POST["email"],
-    //         // 'l' => 'CACI26',
-    //         's' => $_POST["password"],
-    //         // 's' => 'Futuro@123',
-    //         'r' => '1',
-    //         'v' => $hashv,
-    //     ),
-    // ));
-
-    // $response = curl_exec($curl);
-    // // exit($response);
-
-    // // Validar
-    // if (!$response) {
-    //     echo 'Erro cURL: ' . curl_error($curl);
-    //     $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
-    //     header("Location: index.php");
-    //     exit();
-    // }
-
-    // $data = json_decode($response, true);
-
-    // if ($data['error']) {
-    //     $_SESSION['error_message'] = 'Usuário ou senha incorretos. Tente novamente.';
-    //     header("Location: index.php");
-    //     exit();
-    // }
-
-    // // echo $response;
-    // curl_close($curl);
-
-    // 2 - Realizar login novamente
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/login.php', // https://webcred.eaglecred.com.br/ajx/login.php
-        CURLOPT_RETURNTRANSFER => true, // 
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
-        CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
-        CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
-        CURLOPT_POSTFIELDS => array(
-            'l' => $_POST["email"],
-            's' => $_POST["password"],
-            'r' => '1',
-            'v' => $hashv,
-        ),
-    ));
-
-    $response = curl_exec($curl);
-    // exit($response); 
-
-    // Validar
-    if (!$response) {
-        echo 'Erro cURL: ' . curl_error($curl);
-        $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
-        header("Location: index.php");
-        exit();
-    }
-
-    if ($response == 'erro|Login ou senha invalida') {
-        $_SESSION['error_message'] = 'Usuário ou senha incorretos. Tente novamente.';
-        header("Location: index.php");
-        exit();
-    }
-
-
-
-
+    // 2 - Realizar login     
+    login($_POST["email"], $_POST["password"]);
 
     // Validar se enviou produtos
     if ($_POST["products"] == "") {
@@ -186,4 +59,180 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     $_SESSION['success_message'] = 'Usuários de produtos permitidos com sucesso.';
     header("Location: index.php");
     exit();
+}
+
+if (isset($_POST["form_type"]) && $_POST["form_type"] === 'csv') {
+
+    // 1 - Realizar login
+    login($_POST["email"], $_POST["password"]);
+
+    // Pega informações do arquivo
+    $arquivo = $_FILES["csv_file"];
+    $nomeArquivo = $arquivo["name"];
+    $caminhoTemporario = $arquivo["tmp_name"];
+
+    if (pathinfo($nomeArquivo, PATHINFO_EXTENSION) !== "csv") {
+        $_SESSION['error_message'] = 'Apenas arquivos .csv são permitidos.';
+        header("Location: index.php");
+        exit();
+    }
+
+    if (($handle = fopen($caminhoTemporario, "r")) !== false) {
+
+        // Lê cada linha do CSV
+        while (($dados = fgetcsv($handle, 1000, ";")) !== false) {
+            $codigoEnviado = trim($dados[0]);
+            $produtos = explode(',', trim($dados[1])); // Transforma os produtos em array para manipular
+
+            foreach ($produtos as $produto) {
+                $produto = trim($produto);  
+                $produto = str_pad($produto, 6, "0", STR_PAD_LEFT);
+
+                // Buscar produto pelo codigo
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://webcred.eaglecred.com.br/permissoesprodutousuario.php?codproduto=$produto",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'GET',
+                    CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+                    CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+                ));
+
+                $response = curl_exec($curl);
+                // echo $response;
+                // die;
+
+                if (!$response) {
+                    $_SESSION['error_message'] = 'Erro ao autorizar usuários no produto: ' . $produto . '. Tente novamente.';
+                    header("Location: index.php");
+                    exit();
+                }
+
+                curl_close($curl);
+
+                $dom = new DOMDocument();
+                @$dom->loadHTML($response); // Usamos @ para suprimir possíveis warnings de HTML malformado
+
+                $xpath = new DOMXPath($dom);
+
+                $dados = [];
+
+                // Encontra todas as linhas da tabela
+                $rows = $xpath->query('//tr');
+
+                foreach ($rows as $row) {
+                    $checkbox = $xpath->query('.//input[@type="checkbox"]', $row)->item(0);
+                    $cpf_cnpj = $checkbox ? $checkbox->getAttribute('value') : null;
+
+                    $codigo = $xpath->query('.//td[4]', $row)->item(0);
+                    $codigo = $codigo ? trim($codigo->nodeValue) : null;
+
+                    // Se ambos os valores foram encontrados, adiciona ao array
+                    if ($cpf_cnpj && $codigo) {
+                        $dados[] = [
+                            'cpf_cnpj' => $cpf_cnpj,
+                            'codigo' => $codigo
+                        ];
+                    }
+                }
+
+                // Verifica se o código é igual (case insensitive) ou começa com $codigoEnviado (case insensitive)
+                $resultados = array_filter($dados, function ($item) use ($codigoEnviado) {
+                    return strcasecmp($item['codigo'], $codigoEnviado) === 0 || stripos($item['codigo'], $codigoEnviado) === 0;
+                });
+
+                $cpfs_cnpjs = array_column($resultados, 'cpf_cnpj');
+                $cpfs_cnpjs_json = json_encode($cpfs_cnpjs);
+
+                // Autorizar usuários
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/ajx_parametrizaproduto.php',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+                    CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+                    CURLOPT_POSTFIELDS => '{
+                        "CodProduto": "' . $produto . '",
+                        "usuarios": ' . $cpfs_cnpjs_json . ',
+                        "todosForamSelecionados": false,
+                        "process": "permitiremlote"
+                        }',
+                ));
+
+                $response = curl_exec($curl);
+                // die;
+
+                if (!$response) {
+                    $_SESSION['error_message'] = 'Erro ao autorizar usuários no produto: ' . $produto . '. Tente novamente.';
+                    header("Location: index.php");
+                    exit();
+                }
+            }
+        }
+        fclose($handle);
+    } else {
+        $_SESSION['error_message'] = 'Erro ao abrir o arquivo.';
+        header("Location: index.php");
+        exit();
+    }
+
+    $_SESSION['success_message'] = 'Usuários específicos de produtos permitidos com sucesso.';
+    header("Location: index.php");
+    exit();
+}
+
+
+
+function login($email, $password)
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://webcred.eaglecred.com.br/ajx/login.php',
+        CURLOPT_RETURNTRANSFER => true, // 
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_COOKIEJAR => 'cookies.txt',  // Salva os cookies recebidos
+        CURLOPT_COOKIEFILE => 'cookies.txt', // Reutiliza os cookies armazenados
+        CURLOPT_SSL_VERIFYPEER => false,  // Desabilitar verificação SSL
+        CURLOPT_POSTFIELDS => array(
+            'l' => $email,
+            's' => $password,
+            'r' => '1',
+            // 'v' => $hashv,
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    // exit($response);
+
+    // Validar
+    if (!$response) {
+        echo 'Erro cURL: ' . curl_error($curl);
+        $_SESSION['error_message'] = 'Erro inesperado. Tente novamente.';
+        header("Location: index.php");
+        exit();
+    }
+
+    if ($response == 'erro|Login ou senha invalida') {
+        $_SESSION['error_message'] = 'Usuário ou senha incorretos. Tente novamente.';
+        header("Location: index.php");
+        exit();
+    }
 }
--- eaglecred/index.php
+++ eaglecred/index.php
@@ -14,64 +14,224 @@ if (isset($_SESSION['success_message'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <title>Eaglecred</title>
+    <title>Automação Eaglecred</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./jquery.loadingModal.css">
+
 </head>
 
 <body>
     <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="container text-center mx-auto p-4 bg-primary" style="max-width: 350px;">
+        <div class="container text-center mx-auto p-4" style="max-width: 350px; background-color: #1D81C0;">
+            <!-- Campos de login (fora dos formulários) -->
+            <div class="d-flex flex-column align-items-center gap-2 mb-2">
+                <h1 class="text-white">Automação Eaglecred</h1>
+                <input id="email" class="row form-control" type="text" placeholder="Usuario">
+                <div class="input-group">
+                    <input id="password" class="form-control" type="password" placeholder="Senha">
+                    <button class="btn btn-outline" style="border-color: var(--bs-tertiary-bg); color: var(--bs-tertiary-bg);" type="button" onclick="togglePassword()">
+                        <i id="toggleIcon" class="bi bi-eye"></i>
+                    </button>
+                </div>
+            </div>
 
-            <form action="./eagle.php" method="post">
+            <!-- Formulário 1: Autorizar todos os usuários de produto -->
+            <form action="./eagle.php" method="post" onsubmit="copyCredentials(this); showLoadingModal();">
                 <div class="d-flex flex-column align-items-center gap-2">
-                    <h1 class="text-white">Automação Eaglecred</h1>
-                    <input class="row form-control" type="text" name="email" placeholder="Email">
-                    <div class="input-group">
-                        <input id="password" class="form-control" type="password" name="password"
-                            placeholder="Senha">
-                        <button class="btn btn-outline "
-                            style="border-color: var(--bs-tertiary-bg); color: var(--bs-tertiary-bg);" type="button"
-                            onclick="togglePassword()">
-                            <i id="toggleIcon" class="bi bi-eye"></i>
-                        </button>
-                    </div>
-                    <textarea class="form-control mt-2" name="products" placeholder="Produtos (separados por vírgula)"
-                        rows="4">NOME - 12,16</textarea>
+                    <h4 class="text-white">Autorizar todos os usuários de produto:</h4>
+                    <textarea class="form-control mt-2" name="products" placeholder="Produtos (separados por vírgula)" rows="4"></textarea>
                     <button type="submit" class="btn mt-2 form-control" style="background-color: white;">Enviar</button>
                 </div>
+                <!-- Campos ocultos para usuário e senha -->
+                <input type="hidden" name="email">
+                <input type="hidden" name="password">
+                <input type="hidden" name="form_type" value="produtos">
             </form>
 
+            <br>
+
+            <!-- Formulário 2: Autorizar usuários específicos de produtos (upload de CSV) -->
+            <form action="./eagle.php" method="post" enctype="multipart/form-data" onsubmit="copyCredentials(this); showLoadingModal();">
+                <div class="d-flex flex-column align-items-center gap-2">
+                    <h4 class="text-white">Autorizar usuários específicos de produtos:</h4>
+                    <div id="drop-area" class="drop-zone">
+                        <p>Arraste e solte aqui ou clique para selecionar o arquivo</p>
+                        <input type="file" name="csv_file" id="fileInput" accept=".csv" hidden>
+                    </div>
+                    <p id="file-name" class="text-white"></p>
+                    <button type="submit" class="btn mt-2 form-control" style="background-color: white;">Enviar CSV</button>
+                </div>
+                <!-- Campos ocultos para usuário e senha -->
+                <input type="hidden" name="email">
+                <input type="hidden" name="password">
+                <input type="hidden" name="form_type" value="csv">
+            </form>
         </div>
     </div>
 
-
     <script>
-        function togglePassword() {
-            const passwordInput = document.getElementById("password");
-            const icon = document.getElementById("toggleIcon");
+        // Função para copiar os valores dos campos de login para os campos ocultos
+        function copyCredentials(form) {
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
 
-            if (passwordInput.type === "password") {
-                passwordInput.type = "text";
-                icon.classList.remove("bi-eye");
-                icon.classList.add("bi-eye-slash");
+            form.querySelector('input[name="email"]').value = email;
+            form.querySelector('input[name="password"]').value = password;
+        }
+
+        // Função para alternar a visibilidade da senha
+        function togglePassword() {
+            const passwordInput = document.getElementById('password');
+            const toggleIcon = document.getElementById('toggleIcon');
+
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                toggleIcon.classList.remove('bi-eye');
+                toggleIcon.classList.add('bi-eye-slash');
             } else {
-                passwordInput.type = "password";
-                icon.classList.remove("bi-eye-slash");
-                icon.classList.add("bi-eye");
+                passwordInput.type = 'password';
+                toggleIcon.classList.remove('bi-eye-slash');
+                toggleIcon.classList.add('bi-eye');
             }
+        }
+
+        // Lógica para o upload de arquivo
+        const dropArea = document.getElementById('drop-area');
+        const fileInput = document.getElementById('fileInput');
+        const fileName = document.getElementById('file-name');
+
+        dropArea.addEventListener('click', () => fileInput.click());
+
+        fileInput.addEventListener('change', (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                fileName.textContent = `Arquivo selecionado: ${file.name}`;
+            }
+        });
+
+        dropArea.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            dropArea.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
+        });
+
+        dropArea.addEventListener('dragleave', () => {
+            dropArea.style.backgroundColor = 'transparent';
+        });
+
+        dropArea.addEventListener('drop', (e) => {
+            e.preventDefault();
+            dropArea.style.backgroundColor = 'transparent';
+            const file = e.dataTransfer.files[0];
+            if (file) {
+                fileInput.files = e.dataTransfer.files;
+                fileName.textContent = `Arquivo selecionado: ${file.name}`;
+            }
+        });
+
+        function showLoadingModal(mensagem = null) {
+
+            $('body').loadingModal({
+                text: mensagem === null ? 'Carregando...' : mensagem
+            });
+
+            var delay = function(ms) {
+                    return new Promise(function(r) {
+                        setTimeout(r, ms)
+                    })
+                },
+                time = 30000;
+
+            delay(time)
+                .then(function() {
+                    $('body').loadingModal('animation', 'rotatingPlane');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'wave');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'wanderingCubes');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'spinner');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'chasingDots');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'threeBounce');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'circle');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'cubeGrid');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'fadingCircle');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('animation', 'foldingCube');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('color', 'black').loadingModal('text', 'Done :-)');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('hide');
+                    return delay(time);
+                })
+                .then(function() {
+                    $('body').loadingModal('destroy');
+                });
+
+        }
+
+        function hideLoadingModal() {
+            $('body').loadingModal('destroy');
+
+            return true;
+
         }
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+    <script src="//code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <script src="./jquery.loadingModal.js"></script>
+
 </body>
 
 </html>
+
+<style>
+    .drop-zone {
+        border: 2px solid white;
+        padding: 20px;
+        text-align: center;
+        width: 100%;
+        cursor: pointer;
+        color: white;
+        transition: background-color 0.3s;
+    }
+
+    .drop-zone:hover {
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+</style>
